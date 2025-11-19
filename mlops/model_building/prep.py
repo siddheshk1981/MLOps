1fa5a8ca31d6bc573d9c1b954328e9437fd81169
siddheshk1981 <siddhesh.kothari2@gmail.com>
2025-11-19 10:53:11 +0000

    first commit

--- mlops/model_building/prep.py
+++ mlops/model_building/prep.py
@@ -10,7 +10,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/siddhesh1981/bank-customer-churn/bank_customer_churn.csv"
+DATASET_PATH = "hf://datasets/siddhesh1981/bank-customer-churn/bank_customer_churn_latest.csv"
 bank_dataset = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
