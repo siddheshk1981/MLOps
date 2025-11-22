ef0e1a3fcebb6677564b123004c7dbba38449aa4
siddheshk1981 <siddhesh.kothari2@gmail.com>
2025-11-22 00:40:16 +0000

    first commit

--- mlops/model_building/train.py
+++ mlops/model_building/train.py
@@ -22,10 +22,10 @@ mlflow.set_experiment("mlops-training-experiment")
 api = HfApi()
 
 
-Xtrain_path = "hf://datasets/<-----Hugging Face User ID ----->/bank-customer-churn/Xtrain.csv"
-Xtest_path = "hf://datasets/<-----Hugging Face User ID ----->/bank-customer-churn/Xtest.csv"
-ytrain_path = "hf://datasets/<-----Hugging Face User ID ----->/bank-customer-churn/ytrain.csv"
-ytest_path = "hf://datasets/<-----Hugging Face User ID ----->/bank-customer-churn/ytest.csv"
+Xtrain_path = "hf://datasets/siddhesh1981/bank-customer-churn/Xtrain.csv"
+Xtest_path = "hf://datasets/siddhesh1981/bank-customer-churn/Xtest.csv"
+ytrain_path = "hf://datasets/siddhesh1981/bank-customer-churn/ytrain.csv"
+ytest_path = "hf://datasets/siddhesh1981/bank-customer-churn/ytest.csv"
 
 Xtrain = pd.read_csv(Xtrain_path)
 Xtest = pd.read_csv(Xtest_path)
