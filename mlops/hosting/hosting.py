58866e5315b3815c92cd5219f3913d663358eab4
siddheshk1981 <siddhesh.kothari2@gmail.com>
2025-11-22 00:22:24 +0000

    first commit

--- mlops/hosting/hosting.py
+++ mlops/hosting/hosting.py
@@ -6,5 +6,5 @@ api.upload_folder(
     folder_path="mlops/deployment",     # the local folder containing your files
     repo_id="siddhesh1981/Bank-Customer-Churn",          # the target repo
     repo_type="space",                      # dataset, model, or space
-                              # optional: subfolder path inside the repo
+    path_in_repo="",                          # optional: subfolder path inside the repo
 )
